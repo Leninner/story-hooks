031ea73d757b4c10c23f9cb71ec34a88df0a23fe
leninner <mazabandalenin180@gmail.com>
2022-02-09 16:11:38 -0500

    Added useReducer hook

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import { InfoCharacter } from './InfoCharacter';
+
+const initialState = {
+  favorites: [],
+};
+
+const favoriteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITEs':
+      return { ...state, favorites: [...state.favorites, action.payload] };
+
+    default:
+      return state;
+  }
+};
 
 export const Characters = () => {
   const [characters, setCharacters] = useState({ loading: true, data: [] });
+
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+  const handleClick = (character) => {
+    dispatch({ type: 'ADD_TO_FAVORITEs', payload: character });
+  };
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -19,10 +39,23 @@ export const Characters = () => {
   return (
     <section className='w-full bg-[#E6EBEE] dark:bg-slate-700 pt-52'>
       {loading && <p>Loading...</p>}
+      {favorites.favorites.length ? (
+        <>
+          <h2 className='mb-2 text-3xl font-bold text-center tracking-tigh dark:text-white dark:drop-shadow-[0_5px_5px_#e4410084]'>
+            Favorites
+          </h2>
+          <div className='flex flex-wrap justify-center border-b py-50 border-b-slate-300'>
+            {favorites.favorites.map((character) => (
+              <InfoCharacter key={character.id} {...character} withoutButton />
+            ))}
+          </div>
+        </>
+      ) : null}
+
       <div className='flex flex-wrap justify-center py-50'>
         {data.map((character) => {
           const { id } = character;
-          return <InfoCharacter key={id} {...character} />;
+          return <InfoCharacter key={id} {...character} onClick={() => handleClick(character)} />;
         })}
       </div>
     </section>
--- src/components/InfoCharacter.jsx
+++ src/components/InfoCharacter.jsx
@@ -1,10 +1,24 @@
-export const InfoCharacter = ({ name, image, species }) => {
+import { useState } from 'react';
+
+export const InfoCharacter = ({ name, image, species, onClick, withoutButton }) => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleClick = () => {
+    onClick();
+    setIsDisabled(true);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center w-5/6 m-10 md:w-80 '>
       <img src={image} alt='' className='w-[95%] rounded-t' />
       <div className='infoCharacter'>
         <h2 className='mb-2 text-xl font-bold uppercase tracking-tigh'>{name}</h2>
         <p className='text-base text-slate-400'>{species}</p>
+        {!withoutButton && (
+          <button onClick={handleClick} className='addToFavorites' disabled={isDisabled}>
+            +
+          </button>
+        )}
       </div>
     </div>
   );
